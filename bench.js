a9225f921263aa40a8499b01a26aedfc78a3acbd
Roman Shtylman <shtylman@gmail.com>
2012-05-24 17:32:13 -0400

    update benchmark
    - add bunyan
    - show book benchmark with blank, trace, and formatting

--- bench.js
+++ bench.js
@@ -1,29 +1,58 @@
-var book = require('./logger').blank();
-
-var winston = new (require('winston').Logger)({
-    transports: []
-});
-
-function nulldecorator(){};
-book.use(nulldecorator);
-
 function run_sample(name, func) {
     var start = Date.now()/1000.0;
-    for (var i=0 ; i<1000000 ; ++i) {
+    var count = 1000000;
+    for (var i=0 ; i<count ; ++i) {
         func('test');
     }
     var delta = Date.now()/1000.0 - start;
-    console.log(name, delta);
+    var msg_per_sec = count / delta;
+    console.log('%s %d (%d msg/sec)', name, delta, msg_per_sec);
 }
 
-function basic_logging(s) {
-    var entry = {
-        level: 0,
-        message: s.toString(),
-    }
-}
-
+// baseline no-op
 run_sample('no op', function(s) {});
-run_sample('simple', function(s) { basic_logging(s)});
-run_sample('book', function(s) { book.info(s); });
-run_sample('winston', function(s) { winston.info(s); });
+
+(function() {
+    var log = {
+        info: function (s) {
+            var entry = {
+                level: 0,
+                message: s.toString(),
+            }
+        }
+    };
+
+    run_sample('simple', function(s) { log.info(s)});
+})();
+
+(function() {
+    var book = require('./logger');
+    var log = book.default({stdout: false});
+    run_sample('book', function(s) { log.info(s); });
+    run_sample('book-format', function(s) { log.info('cat: %s', s); });
+
+    var log = book.default({trace: book.TRACE, stdout: false});
+    run_sample('book-trace', function(s) { log.info(s); });
+
+    var log = require('./logger').blank();
+    log.use(function(){});
+    run_sample('book-blank', function(s) { log.info(s); });
+})();
+
+(function() {
+    var log = new (require('winston').Logger)();
+    run_sample('winston', function(s) { log.info(s); });
+})();
+
+(function() {
+    var null_stream = {
+        write: function() {}
+    };
+
+    var bunyan = require('bunyan');
+    var log = bunyan.createLogger({name: 'myapp', stream: null_stream});
+
+    run_sample('bunyan', function(s) { log.info(s); });
+    run_sample('bunyan-format', function(s) { log.info('cat: %s', s); });
+})();
+
